ab2238b62bb699fb62684d6e29a8ffadb1cc7296
Vinícius de Bona Ruas <vinicius_ruas@estudante.sesisenai.org.br>
2025-02-12 11:53:48 -0300

    Criada a classe cone

--- Cilindro.js
+++ Cilindro.js
@@ -1,15 +1,15 @@
 export default class Cillindro {
     constructor(raio, altura) {
-        this.raio = raio;
-        this.altura = altura;
+        this.raio = raio
+        this.altura = altura
     }
 
     calcularVolume() {
-        return Math.PI * (this.raio ^ 2) * this.altura;
+        return Math.PI * Math.pow(this.raio,2) * this.altura
     }
 
     calcularAreaTotal() {
-        return (2 * (Math.PI * (this.raio ^ 2)) + 
-                2 * (Math.PI * this.raio * this.altura));
+        return (2 * (Math.PI * Math.pow(this.raio,2)) + 
+                2 * (Math.PI * this.raio * this.altura))
     }
 }
